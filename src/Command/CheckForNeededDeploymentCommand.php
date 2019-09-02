9e0d490526e22b473f56a51f1fdb6eae1a7aacf7
narcoticfresh <dn@xbe.ch>
2019-09-02 11:32:30 +0200

    initial structure

---
+++ src/Command/CheckForNeededDeploymentCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Command that checks if we need to run database stuff on the current deploy
+ */
+
+namespace Graviton\DeploymentServiceBundle\Command;
+
+/**
+ * @author  List of contributors <https://github.com/libgraviton/DeploymentServiceBundle/graphs/contributors>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+class CheckForNeededDeploymentCommand extends Command
+{
+
+}
